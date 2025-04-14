0e2af97705ab9f202346917aaf845bb596e37152
Johann Becker <griesgra@gmail.com>
2025-04-14 12:52:23 +0200

    Implement state machine for 4-digit input and timer functionality, including alarm state and segment display updates

--- lib/FK04_HM_MFS/src/mfs.c
+++ lib/FK04_HM_MFS/src/mfs.c
@@ -3,29 +3,127 @@
 #include <stddef.h>
 #include "mfs.h"
 
-
 /* Segment byte maps for numbers 0 to 9  with ' '  and '-'*/
 /* Explanation https://c2plabs.com/blog/2020/09/13/arduino-multi-function-shield-seven-segment-display/ */
 const uint8_t SEGMENT_MAP[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, /* 0 - 9 */
-                               0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E, 
+                               0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E,
                                0xBF, 0xFF};
 /* Byte maps to select digit 1 to 4 */
+const uint8_t SevenSegmentASCII[96] = {
+    0x00, /* (space) */
+    0x86, /* ! */
+    0x22, /* " */
+    0x7E, /* # */
+    0x6D, /* $ */
+    0xD2, /* % */
+    0x46, /* & */
+    0x20, /* ' */
+    0x29, /* ( */
+    0x0B, /* ) */
+    0x21, /* * */
+    0x70, /* + */
+    0x10, /* , */
+    0x40, /* - */
+    0x80, /* . */
+    0x52, /* / */
+    0x3F, /* 0 */
+    0x06, /* 1 */
+    0x5B, /* 2 */
+    0x4F, /* 3 */
+    0x66, /* 4 */
+    0x6D, /* 5 */
+    0x7D, /* 6 */
+    0x07, /* 7 */
+    0x7F, /* 8 */
+    0x6F, /* 9 */
+    0x09, /* : */
+    0x0D, /* ; */
+    0x61, /* < */
+    0x48, /* = */
+    0x43, /* > */
+    0xD3, /* ? */
+    0x5F, /* @ */
+    0x77, /* A */
+    0x7C, /* B */
+    0x39, /* C */
+    0x5E, /* D */
+    0x79, /* E */
+    0x71, /* F */
+    0x3D, /* G */
+    0x76, /* H */
+    0x30, /* I */
+    0x1E, /* J */
+    0x75, /* K */
+    0x38, /* L */
+    0x15, /* M */
+    0x37, /* N */
+    0x3F, /* O */
+    0x73, /* P */
+    0x6B, /* Q */
+    0x33, /* R */
+    0x6D, /* S */
+    0x78, /* T */
+    0x3E, /* U */
+    0x3E, /* V */
+    0x2A, /* W */
+    0x76, /* X */
+    0x6E, /* Y */
+    0x5B, /* Z */
+    0x39, /* [ */
+    0x64, /* \ */
+    0x0F, /* ] */
+    0x23, /* ^ */
+    0x08, /* _ */
+    0x02, /* ` */
+    0x5F, /* a */
+    0x7C, /* b */
+    0x58, /* c */
+    0x5E, /* d */
+    0x7B, /* e */
+    0x71, /* f */
+    0x6F, /* g */
+    0x74, /* h */
+    0x10, /* i */
+    0x0C, /* j */
+    0x75, /* k */
+    0x30, /* l */
+    0x14, /* m */
+    0x54, /* n */
+    0x5C, /* o */
+    0x73, /* p */
+    0x67, /* q */
+    0x50, /* r */
+    0x6D, /* s */
+    0x78, /* t */
+    0x1C, /* u */
+    0x1C, /* v */
+    0x14, /* w */
+    0x76, /* x */
+    0x6E, /* y */
+    0x5B, /* z */
+    0x46, /* { */
+    0x30, /* | */
+    0x70, /* } */
+    0x01, /* ~ */
+    0x00, /* (del) */
+};
 const uint8_t SEGMENT_SELECT[] = {0x1, 0x2, 0x4, 0x8};
 
-const size_t SEGMENT_MAP_LEN=sizeof(SEGMENT_MAP)/sizeof(uint8_t);
+const size_t SEGMENT_MAP_LEN = sizeof(SEGMENT_MAP) / sizeof(uint8_t);
 
-void initMFS(void){
-  pinMode(S1,INPUT);
-  pinMode(S2,INPUT);
-  pinMode(S3,INPUT);
-  pinMode(POTI,INPUT);
+void initMFS(void)
+{
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(POTI, INPUT);
 
   digitalWrite(BUZZER, BUZZER_OFF);
   digitalWrite(LED1, MFS_LOW);
   digitalWrite(LED2, MFS_LOW);
   digitalWrite(LED3, MFS_LOW);
   digitalWrite(LED4, MFS_LOW);
-  
+
   pinMode(LATCH_DIO, OUTPUT);
   pinMode(CLK_DIO, OUTPUT);
   pinMode(DATA_DIO, OUTPUT);
@@ -44,49 +142,56 @@ void initMFS(void){
 
 void writeDecToSegment(int val)
 {
-  int i, end=0;
-  bool ok=true;
-  if (val<0) {
-	  end=1;
-	  val=-val;
+  int i, end = 0;
+  bool ok = true;
+  if (val < 0)
+  {
+    end = 1;
+    val = -val;
   }
 
-  for (i=3; ok && i>=end; --i) {
-  	writeCharToSegment(i, val % 10, false);
-  	val/=10;
-    if (val==0)
-      ok=false;
+  for (i = 3; ok && i >= end; --i)
+  {
+    writeCharToSegment(i, val % 10, false);
+    val /= 10;
+    if (val == 0)
+      ok = false;
   }
-  
-  if (end==1)		// number was negative
+
+  if (end == 1) // number was negative
     writeCharToSegment(i--, SEGMENT_DASH, false);
 
   // convert leading zeros to blanks
-  for(;i>=0;--i)
+  for (; i >= 0; --i)
     writeCharToSegment(i, SEGMENT_CLEAR, false);
-  
 }
 
 void writeHexToSegment(unsigned val)
 {
   int i;
-	
-  for (i=3; i>=0; --i) {
-	  writeCharToSegment(i, val % 16, false);
-	  val/=16;
+
+  for (i = 3; i >= 0; --i)
+  {
+    writeCharToSegment(i, val % 16, false);
+    val /= 16;
   }
-  
 }
 
 void writeCharToSegment(uint8_t segment, uint8_t value, int dot)
 {
-  uint8_t dataout=SEGMENT_MAP[SEGMENT_MAP_LEN-1];
+  uint8_t dataout = SEGMENT_MAP[SEGMENT_MAP_LEN - 1];
 
-  if (value>=0 && value<SEGMENT_MAP_LEN)
-		dataout=SEGMENT_MAP[value];
+  if (value >= 0 && value < SEGMENT_MAP_LEN)
+  {
+    dataout = SEGMENT_MAP[value];
+  }
+  else
+  {
+    dataout = ~SevenSegmentASCII[value - 32]; // ASCII to segment map
+  }
 
-  digitalWrite(LATCH_DIO,LOW);
+  digitalWrite(LATCH_DIO, LOW);
   shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, dataout & (dot ? 0x7F : 0xFF));
   shiftOut(DATA_DIO, CLK_DIO, MSBFIRST, SEGMENT_SELECT[segment]);
-  digitalWrite(LATCH_DIO,HIGH);
+  digitalWrite(LATCH_DIO, HIGH);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,257 @@
 #include "mfs.h"
 #include <Arduino.h>
 #define T_WAIT 50
-int main(void) {
-init();
-initMFS();
-int T = 2000; 
-int wait_o = 0; 
-while (1) {
-wait_o = analogRead(POTI);
-int wait_p = T - 1.9*wait_o; 
-digitalWrite(LED2, MFS_HIGH);
-delayMicroseconds(wait_o);
-digitalWrite(LED2, MFS_LOW);
-delayMicroseconds(wait_p);
+
+enum status
+{
+    FIRST_DIGIT,
+    SECOND_DIGIT,
+    THIRD_DIGIT,
+    FOURTH_DIGIT,
+    INPUT_FINISHED,
+    START_TIMER,
+    ALARM
+};
+
+int inc_value(int x)
+{
+    if (x < 9 && x >= 0)
+    {
+        x++;
+    }
+    else
+    {
+        x = 0;
+    }
+
+    return x;
 }
-return 0;
+
+int dec_value(int x)
+{
+    if (x < 10 && x > 0)
+    {
+        x--;
+    }
+    else
+    {
+        x = 9;
+    }
+
+    return x;
+}
+
+int main(void)
+{
+    init();
+    initMFS();
+    uint8_t digit1 = 0, digit2 = SEGMENT_CLEAR, digit3 = SEGMENT_CLEAR, digit4 = SEGMENT_CLEAR;
+
+    status state = FIRST_DIGIT;
+    uint8_t s1_old = 0;
+    uint8_t s2_old = 0;
+    uint8_t s3_old = 0;
+    Serial.begin(9600);
+    unsigned long timer = 0;
+    bool punkt = false;
+    bool ganzzahl = false;
+    uint32_t ausgabe_ganzzahl = 0;
+    uint32_t time = 0;
+    while (1)
+    {
+
+        uint8_t s1 = digitalRead(S1);
+        uint8_t s2 = digitalRead(S2);
+        uint8_t s3 = digitalRead(S3);
+        switch (state)
+        {
+        case FIRST_DIGIT:
+        {
+            if (s1 < s1_old)
+            {
+                digit1 = inc_value(digit1);
+            }
+            if (s2 < s2_old)
+            {
+                digit1 = dec_value(digit1);
+            }
+            if (s3 < s3_old)
+            {
+                state = SECOND_DIGIT;
+                digit2 = 0;
+            }
+            break;
+        }
+
+        case SECOND_DIGIT:
+        {
+            if (s1 < s1_old)
+            {
+                digit2 = inc_value(digit2);
+            }
+            if (s2 < s2_old)
+            {
+                digit2 = dec_value(digit2);
+            }
+            if (s3 < s3_old)
+            {
+                state = THIRD_DIGIT;
+                digit3 = 0;
+            }
+            break;
+        }
+
+        case THIRD_DIGIT:
+        {
+            if (s1 < s1_old)
+            {
+                digit3 = inc_value(digit3);
+            }
+            if (s2 < s2_old)
+            {
+                digit3 = dec_value(digit3);
+            }
+            if (s3 < s3_old)
+            {
+                state = FOURTH_DIGIT;
+                digit4 = 0;
+            }
+            break;
+        }
+
+        case FOURTH_DIGIT:
+        {
+            if (s1 < s1_old)
+            {
+                digit4 = inc_value(digit4);
+            }
+            if (s2 < s2_old)
+            {
+                digit4 = dec_value(digit4);
+            }
+            if (s3 < s3_old)
+            {
+                state = INPUT_FINISHED;
+                punkt = true;
+            }
+            /* code */
+            break;
+        }
+
+        case INPUT_FINISHED:
+        {
+            if (s1 < s1_old)
+            {
+                state = FIRST_DIGIT;
+                digit1 = 0;
+                digit2 = SEGMENT_CLEAR;
+                digit3 = SEGMENT_CLEAR;
+                digit4 = SEGMENT_CLEAR;
+                punkt = false;
+            }
+            if (s2 < s2_old)
+            {
+                /* code */
+            }
+            if (s3 < s3_old)
+            {
+                timer = (digit1 * 1000 + digit2 * 100 + digit3 * 10 + digit4);
+                ausgabe_ganzzahl = round(timer / 1000);
+                state = START_TIMER;
+                punkt = false;
+                ganzzahl = true;
+                time = millis();
+            }
+            /* code */
+            break;
+        }
+
+        case START_TIMER:
+        {
+            {
+                uint64_t time_passed = millis() - time;
+                if (time_passed > 1000)
+                {
+                    timer--;
+                    time = millis();
+                }
+                ausgabe_ganzzahl = timer;
+            }
+            if (s1 < s1_old)
+            {
+            }
+            if (s3 < s3_old)
+            {
+                state = FIRST_DIGIT;
+                digit1 = 0;
+                digit2 = SEGMENT_CLEAR;
+                digit3 = SEGMENT_CLEAR;
+                digit4 = SEGMENT_CLEAR;
+                punkt = false;
+                ganzzahl = false;
+                digitalWrite(LED1, HIGH);
+            }
+
+            if (ausgabe_ganzzahl < 1)
+            {
+                state = ALARM;
+                time = millis();
+            }
+            break;
+        }
+        case ALARM:
+        {
+            digit1 = 'A';
+            digit2 = 'L';
+            digit3 = 'R';
+            digit4 = 'M';
+            ganzzahl = false;
+
+            int passed_time = millis() - time;
+            if (passed_time < 500)
+            {
+                digitalWrite(LED1, LOW);
+                punkt = false;
+            }
+            else
+            {
+                digitalWrite(LED1, HIGH);
+                punkt = true;
+            }
+            if (passed_time > 1000)
+            {
+                time = millis();
+            }
+            if (s3 < s3_old)
+            {
+                state = FIRST_DIGIT;
+                digit1 = 0;
+                digit2 = SEGMENT_CLEAR;
+                digit3 = SEGMENT_CLEAR;
+                digit4 = SEGMENT_CLEAR;
+                punkt = false;
+                digitalWrite(LED1, HIGH);
+            }
+            break;
+        }
+        default:
+            break;
+        }
+        if (ganzzahl)
+        {
+            writeDecToSegment(ausgabe_ganzzahl);
+        }
+        else
+        {
+            writeCharToSegment(0, digit1, punkt);
+            writeCharToSegment(1, digit2, punkt);
+            writeCharToSegment(2, digit3, punkt);
+            writeCharToSegment(3, digit4, punkt);
+        }
+
+        s1_old = s1;
+        s2_old = s2;
+        s3_old = s3;
+    }
+    return 0;
 }
